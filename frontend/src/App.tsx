ddb066b544f19373b5cb661d7b2f837308cd5f98
serdeshnow <vserdes1977@mail.ru>
2024-11-15 15:45:33 +0300

    initial frontend commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+// import ReactLogo from './assets/svg/react.svg?react'; // svgr plugin usage
+// import viteLogo from '/vite.svg';
+import './styles/App.scss';
+
+export const App: FC = () => {
+
+	return (
+		<>
+		</>
+	);
+};
