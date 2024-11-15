4c8c54602d47bd350b238f357ec7fa96aa8714e8
serdeshnow <vserdes1977@mail.ru>
2024-11-16 02:38:44 +0300

    what

--- frontend/src/pages/RootLayout/components/Header/Header.tsx
+++ frontend/src/pages/RootLayout/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import { AppBar, SvgIcon } from '@mui/material';
+import { AppBar, SvgIcon, Toolbar } from '@mui/material';
 
 export const Header: FC = () => {
 	return <AppBar className="header">
-
-		<SvgIcon>
-			<CalendarMonthOutlinedIcon/>
-		</SvgIcon>
+		<Toolbar>
+			<SvgIcon>
+				<CalendarMonthOutlinedIcon/>
+			</SvgIcon>
+		</Toolbar>
 	</AppBar>;
 };
